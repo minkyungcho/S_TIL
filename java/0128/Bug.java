037e8f54b1baa8ccb8deb7fa15a4c5e2d30aa8f3
ming_Mac <xiuperjjo@naver.com>
2020-01-28 23:34:59 +0900

    bug

--- java/0128/Bug.java
+++ java/0128/Bug.java
@@ -9,53 +9,50 @@ public class Bug {
 	static int T, size, num;
 	static int answer = 0;
 	
+	public static void printMap(int[][] map, int N) {
+    	for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+    }
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		System.setIn(new FileInputStream("res/Bug.txt"));
 		Scanner sc = new Scanner(System.in);
-		// 하  우 
+		// down right 
 		int[] dirR = {1, 0};
 		int[] dirC = {0, 1};
-		T = sc.nextInt(); // testcase 입력
+		T = sc.nextInt(); // testcase 
 		for(int testCase=1; testCase<T+1; testCase++) {
-			// 입력
+
 			size = sc.nextInt();
 			num = sc.nextInt();
 			int[][] map = new int[size][size];
-			
+			answer = 0;
 			for(int n=1; n<num+1; n++) {
 				int number = n;
 				int r = sc.nextInt();
 				int c = sc.nextInt();
 				int dir = sc.nextInt(); // dir-1
-				
-				int nr = r + dirR[dir]*3;
-				int nc = c + dirC[dir]*3;
-
-				int nnr = nr + dirR[dir]*2;
-				int nnc = nc + dirC[dir]*2;
-				
-				int nnnr = nnr + dirR[dir];
-				int nnnc = nnc + dirC[dir];
-				
-				// 점프 가능 : 경계범위 내, 다음 지점에 소금쟁이 없음.  
-				if(nr>-1 && nr<size && nc>-1 && nc<size && map[nr][nc]==0) {
-					map[nr][nc] = n;
-				}else continue;
-
-				if(nnr>-1 && nnr<size && nnc>-1 && nnc<size && map[nnr][nnc]==0) {
-					map[nnr][nnc] = n;
-				}else continue;
-				
-				if(nnnr>-1 && nnnr<size && nnnc>-1 && nnnc<size && map[nnnr][nnnc]==0) {
-					map[nnnr][nnnc] = n;
-				}else continue;
-				
-				
-				
+				for(int i=3; i>0; i--) {
+					int nr = r + dirR[dir-1]*i;
+					int nc = c + dirC[dir-1]*i;
+					r = nr;
+					c = nc;
+					if(nr>-1 && nr<size && nc>-1 && nc<size) {
+						if(map[nr][nc]==0)
+							map[nr][nc] = n;
+						else 
+							answer = n;
+					}
+				}
 			}
-			
+//			printMap(map, size);
 			System.out.println("#"+testCase+" "+answer);
+//			System.out.println("=====================");
 		}
 		
 	}
